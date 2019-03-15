1b6e52c3b00867ae2b5faaf4568e39a02ac082ca
Jalia Evans <irohdesign@outlook.com>
2019-03-15 15:27:59 -0400

    working on scraping from NIH with puppeteer

---
+++ scrape.js
@@ -0,0 +1,38 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://druginfo.nlm.nih.gov/drugportal/drug/names');
+
+//   grab drug names from page
+  const drugNames = await page.evaluate(() => Array.from(document.querySelector('table tbody').childNodes[2].querySelector('tbody').childNodes[6].childNodes[0].childNodes[0].childNodes[1].querySelectorAll('tr')).map((row) => row.innerText));
+  const innerText = await page.evaluate(() => document.querySelector('table a').innerText);
+
+//   get rid of drug names header from table
+  drugNames.shift();
+
+  let drugList = [];
+    
+  for (var i = 0; i < drugNames.length; i++) {
+    let drug = new Object();
+    drug.name = drugNames[i];
+
+    drugList.push(drug);
+    }
+
+    JSON.stringify(drugList);
+
+  console.log(innerText);
+
+await fs.writeFile('./results/drugNames.json', drugNames, function (error) {
+    if (error) {
+        console.log('something went wrong, oops!');
+    } else {
+        console.log('Wrote to file!')
+    }
+})
+
+  browser.close();
+})();

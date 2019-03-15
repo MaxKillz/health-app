05a0d7eeaa5bc68d44f20b6476b35eff8d2e0e6e
Jalia Evans <irohdesign@outlook.com>
2019-03-15 15:41:46 -0400

    successfully scraped drug names from NIH, all that start with A, and wrote it into JSON file as individual objects

--- scrape.js
+++ scrape.js
@@ -1,38 +1,44 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
+const puppeteer = require("puppeteer");
+const fs = require("fs");
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://druginfo.nlm.nih.gov/drugportal/drug/names');
+  await page.goto("https://druginfo.nlm.nih.gov/drugportal/drug/names");
 
-//   grab drug names from page
-  const drugNames = await page.evaluate(() => Array.from(document.querySelector('table tbody').childNodes[2].querySelector('tbody').childNodes[6].childNodes[0].childNodes[0].childNodes[1].querySelectorAll('tr')).map((row) => row.innerText));
-  const innerText = await page.evaluate(() => document.querySelector('table a').innerText);
+  //   grab drug names from page
+  const drugNames = await page.evaluate(() =>
+    Array.from(
+      document
+        .querySelector("table tbody")
+        .childNodes[2].querySelector("tbody")
+        .childNodes[6].childNodes[0].childNodes[0].childNodes[1].querySelectorAll(
+          "tr"
+        )
+    ).map(row => row.innerText)
+  );
+  const innerText = await page.evaluate(
+    () => document.querySelector("table a").innerText
+  );
 
-//   get rid of drug names header from table
+  //   get rid of drug names header from table
   drugNames.shift();
 
-  let drugList = [];
-    
-  for (var i = 0; i < drugNames.length; i++) {
-    let drug = new Object();
-    drug.name = drugNames[i];
+    let drugList = {}
 
-    drugList.push(drug);
+    for (var i = 0; i < drugNames.length; i++) {
+        drugList[i] = {
+            name: drugNames[i]
+        }
     }
 
-    JSON.stringify(drugList);
-
-  console.log(innerText);
-
-await fs.writeFile('./results/drugNames.json', drugNames, function (error) {
+  await fs.writeFile("./results/drugNames.json", JSON.stringify(drugList, null, 2), function(error) {
     if (error) {
-        console.log('something went wrong, oops!');
+      console.log("something went wrong, oops!");
     } else {
-        console.log('Wrote to file!')
+      console.log("Wrote to file!");
     }
-})
+  });
 
   browser.close();
 })();

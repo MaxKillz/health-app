47cbdc53526a6b6b551a7fc860762dd0f253084a
Jalia Evans <irohdesign@outlook.com>
2019-03-15 17:21:30 -0400

    scraper works for both names and objects, running through each page of the NIH listing (separate pages from A-Z)

--- scrape.js
+++ scrape.js
@@ -4,10 +4,22 @@ const fs = require("fs");
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  
   await page.goto("https://druginfo.nlm.nih.gov/drugportal/drug/names");
 
+  let drugNames = [];
+  let drugList = {};
+  let numberOfPages = ['a','b','c','d','e','f','g','h','i','j','k','l','m', 'n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+
   //   grab drug names from page
-  const drugNames = await page.evaluate(() =>
+  for (var p = 0; p < numberOfPages.length; p++) {
+
+    let url = (`https://druginfo.nlm.nih.gov/drugportal/drug/names/${numberOfPages[p]}`)
+
+    await page.goto(url);
+
+    let drug = await page.evaluate(() =>
     Array.from(
       document
       .querySelector("table tbody")
@@ -18,18 +30,39 @@ const fs = require("fs");
     ).map(row => row.innerText)
   );
 
-  //   get rid of drug names header from table
-  drugNames.shift();
+   //   get rid of drug names header from table
+    drug.shift();
 
-  let drugList = {}
+    drugNames.push(drug);
+  }
 
-  for (var i = 0; i < drugNames.length; i++) {
-    drugList[i] = {
-      name: drugNames[i]
-    }
+  drugNames = drugNames.reduce(function(a, b) {
+    return a.concat(b);
+  }, []);
+
+  drugList = {
+    name: drugNames
   }
 
   await fs.writeFile("./results/drugNames.json", JSON.stringify(drugList, null, 2), function (error) {
+    if (error) {
+      console.log("something went wrong, oops!");
+    } else {
+      console.log("Wrote to file!");
+    }
+  });
+
+  for (var i = 0; i < drugNames.length; i++) {
+    drugNames[i] = {
+      name: drugNames[i]
+    };
+  }
+
+  drugList = {
+    drugs: drugNames
+  }
+
+  await fs.writeFile("./results/drugObjects.json", JSON.stringify(drugList, null, 2), function (error) {
     if (error) {
       console.log("something went wrong, oops!");
     } else {
